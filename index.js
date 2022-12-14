ec7faf83b0c23ff103ed06ad44389179a861a469
Aura <ax.pink@gmail.com>
2022-12-14 10:50:25 -0800

    commit tree 9d23d9215cd3bc86fab649909270bdc0ada5f57b

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name) {
+    return cats.push(name)
+}
+function destructivelyPrependCat(name) {
+    return cats.unshift(name)
+}
+function destructivelyRemoveLastCat() {
+    return cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    return cats.shift()
+}
+function appendCat(name) {
+    const catsCopy = cats.slice()
+    catsCopy.push(name)
+    return catsCopy
+}
+function prependCat(name) {
+    const catsCopy = cats.slice()
+    catsCopy.unshift(name)
+    return catsCopy
+}
+function removeLastCat() {
+    const catsCopy = cats.slice()
+    catsCopy.pop()
+    return catsCopy
+}
+function removeFirstCat() {
+    const catsCopy = cats.slice()
+    catsCopy.shift()
+    return catsCopy
+}
 // Write your solution here!
